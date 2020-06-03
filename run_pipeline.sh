d66c67858255b4bd54e5fbe108e3e2b954fc40dd
bballew <bari.ballew@nih.gov>
2020-06-03 15:12:49 -0400

    update dependencies

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # set your environment here, e.g.:
-module load python3/3.7.0 sge perl singularity/3.0.1
+module load python3/3.7.0 sge perl singularity/3.0.1 git
 
 # edit submission command as desired:
-cmd="qsub -q long.q -V -j y -S /bin/sh -o ${PWD} ${PWD}/SV_wrapper.sh ${PWD}/config.yaml"
+cmd="qsub -q long.q -V -j y -S /bin/sh -o ${PWD} path/to/SV_wrapper.sh ${PWD}/config.yaml"
 echo "Command run: $cmd"
 eval $cmd
