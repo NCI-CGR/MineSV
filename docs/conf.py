10a8c8b955b2fe5b2222b5338511ef2ac8199c2c
Wei Zhu <zhuw10@nih.gov>
2021-08-12 17:19:36 -0400

    add more sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,9 @@ release = '0.4'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'm2r2'
+    'm2r2',    
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
