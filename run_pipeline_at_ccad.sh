dbc39ff4777ddb1ee0d3fe7b21da9635bf66820e
Wei Zhu <zhuw10@nih.gov>
2021-08-13 10:19:43 -0400

    add document

--- run_pipeline_at_ccad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# set your environment here, e.g.:
-module load  git/2.12.0 python3/3.7.0 sge/2011.11p1 perl/5.32.1 singularity/3.0.1
-
-# edit submission command as desired:
-cmd="qsub -q long.q -V -j y -S /bin/sh -o ${PWD} ${PWD}/SV_wrapper.sh ${PWD}/config.yaml"
-echo "Command run: $cmd"
-eval $cmd
--- scripts/list_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-inFile=$1
-sample=$2
-
-echo $inFile | tr " " "\n" | sed "s/^/$sample /"
